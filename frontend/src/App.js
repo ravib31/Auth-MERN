e97d9224652a3450d7ac35cb58c39286618d3fbd
ravib31 <rkb0808@gmail.com>
2024-08-11 00:37:44 +0530

    basic frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Auth App</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+      </Routes>
     </div>
   );
 }
