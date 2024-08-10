fae747966ca6d33c1018f98e00d1e28ed0573cb5
ravib31 <rkb0808@gmail.com>
2024-08-11 02:59:57 +0530

    signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { errorToast, successToast } from "../utils";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [signupInfo, setSignupInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-export default Signup
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSignupInfo({ ...signupInfo, [e.target.name]: e.target.value });
+  };
+  console.log(signupInfo);
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    // console.log(signupInfo)
+    // const {name,email,password}=signupInfo;
+    if (
+      signupInfo.name === "" ||
+      signupInfo.email === "" ||
+      signupInfo.password === ""
+    ) {
+      return errorToast("All fields are required");
+    }
+    try {
+      const signupUrl = "http://localhost:8080/auth/signup";
+      const response = await fetch(signupUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupInfo),
+      });
+      const res = await response.json();
+      const { success, message } = res;
+      if (success) {
+        successToast(message);
+        // setSignupInfo({
+        //     name:"",
+        //     email:"",
+        //     password:""
+        // });
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+        // return successToast(message)
+      } else {
+        errorToast(message);
+      }
+      // console.log(res)
+    } catch (error) {
+      errorToast("Something went wrong");
+    }
+  };
+  return (
+    <div className="container">
+      <h1>Signup</h1>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor="Name">Name</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            value={signupInfo.name}
+            name="name"
+            id="name"
+            autoFocus
+            placeholder="Enter Your Name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            value={signupInfo.email}
+            name="email"
+            id="email"
+            autoFocus
+            placeholder="Enter Your Email..."
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChange}
+            type="password"
+            value={signupInfo.password}
+            name="password"
+            id="password"
+            autoFocus
+            placeholder="Enter Your Password"
+          />
+        </div>
+
+        <div>
+          <button type="submit">Signup</button>
+        </div>
+        <div>
+          <p>
+            Don't have an account? <a href="/login">Login</a>
+          </p>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Signup;
---
+++ frontend/src/utils.js
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+
+
+export const successToast = (message) => {
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
+}
+
+export const errorToast = (message) => {
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
+}
