88458144143c7aaa6f7819128e93be439371610e
ravib31 <rkb0808@gmail.com>
2024-08-12 16:09:21 +0530

    complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,29 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import RefreshHandler from './RefreshHandler';
+import { useState } from 'react';
 
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
   return (
     <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path='/' element={<Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   );
 }
+
 
 export default App;
